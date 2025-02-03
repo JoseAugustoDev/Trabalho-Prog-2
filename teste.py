fa80cd83db3cbfab8260da5094c016a742674dec
Jose Augusto Costa <joseaugustodeveloper@gmail.com>
2025-02-02 23:04:33 -0300

    Tentando utilizar o dicionario

---
+++ teste.py
@@ -0,0 +1,22 @@
+ # with open("dicionario-exemplo.bin", "rb") as f:
+    #     alunos = pickle.load(f)
+
+    # # print(alunos)
+
+    # l = []
+
+    # for matricula in alunos:
+        
+    #     notas = alunos[matricula][1:]
+    #     nome_aluno = alunos[matricula][0]
+
+    #     n1 = notas[0]
+    #     n2 = notas[1]
+    #     n3 = notas[2]
+    #     n4 = notas[3]
+
+    #     tempo = notas[4]
+
+    #     nota = n1 + n2 + n3 + n4
+
+    #     l.append((nome_aluno, nota, tempo))
